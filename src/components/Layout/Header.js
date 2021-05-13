1f412f424d887b866c2bc33b4cba384abf76041b
rickycheah <rickyclk15@gmail.com>
2021-05-13 16:53:37 -0400

    added Cart button, icon, and badge.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,14 +2,16 @@ import React, { Fragment } from "react";
 
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
+        <HeaderCartButton />
       </header>
-      <div className={classes['main-image']}>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A banquet!" />
       </div>
     </Fragment>
