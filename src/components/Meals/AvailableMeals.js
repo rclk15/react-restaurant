15e720a2de52f4062af6f6ad03a59cc901563ef2
rickycheah <rickyclk15@gmail.com>
2021-06-23 14:39:59 -0400

    Add loading text and error catchingfor available meals.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,16 +32,25 @@ import MealItem from "./MealItem/MealItem";
 // ];
 
 const AvailableMeals = (props) => {
+  console.log("meals");
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(); //undefined
 
   console.log("available meals");
   useEffect(() => {
     console.log("effect");
+
     const getMeals = async () => {
-      const data = await fetch(
-        "https://react-custom-73305-default-rtdb.firebaseio.com/meals.json"
+      const response = await fetch(
+        "https://react-custom-73305-default-rtdb.firebaseio.com/meals"
       );
-      const dataJson = await data.json();
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("Something went wrong!");
+        // lines after this won't execute!
+      }
+      const dataJson = await response.json();
       console.log(dataJson);
 
       let loadedMeals = [];
@@ -53,19 +62,52 @@ const AvailableMeals = (props) => {
           name: dataJson[key].name,
           description: dataJson[key].description,
           price: dataJson[key].price,
-        })
+        });
       }
+      console.log(1);
       setMeals(loadedMeals);
+      console.log(2);
+      setIsLoading(false);
     };
-    getMeals();
+    // try {
+    //   // since getMeals is async, but we can't await here b/c useEffect cannot
+    //   // take async function. have to use .catch instead of just catch for the
+    //   // error thrown in getMeals. A rejected promise will be returned.
+    //   getMeals();
+    // } catch {
+    //   setIsLoading(false);
+    //   setError(error.message); // this message is what we set above!
+    // }
+    getMeals().catch((error) => {
+      console.log(3);
+      setIsLoading(false);
+      console.log(4);
+      setError(error.message);
+    });
   }, []);
 
-  const mealList = meals.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+  if (isLoading) {
+    return (
+      <section className={classes.loadingmeals}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{error}</p>
+      </section>
+    );
+  }
+
+  const mealList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
 
   return (
     <section className={classes.meals}>
+      {/* {isLoading && <section><p>Loading!</p></section>}
+      {isLoading ? console.log("test") : console.log("done")} */}
       <Card>
         <ul>{mealList}</ul>
       </Card>
